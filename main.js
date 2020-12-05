d7c9d35d8f4c0d2ae4e8fb212ddf6b6d9cc31b6b
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2020-12-05 16:36:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function galaxy(){
+    window.alert("Never apologize for burning too brightly or collapsing into yourself every night. That is how galaxies are made");
+}
+function plant(){
+    window.alert("Whatever we plant in our subconscious mind and nourish with repetition and emotion will one day become a reality.");
+}
+function unity(){
+    window.alert("If you want to go quickly go alone. If you want to go far go together.");
+}
+function science(){
+window.alert("Everything is theoretically impossible, until it is done.");   
+}
+function technology(){
+    window.alert("It is only when they go wrong that machines remind you how powerful they are.");
+}
+function diversity(){
+    window.alert(" When we listen and celebrate what is both common and different, we become a wiser, more inclusive, and better organization.")
+}
